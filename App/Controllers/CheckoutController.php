9364fcdbda5fdc32fc0013720e043091ce57a1d8
Vokurkaa <zadrobilek.jan@seznam.cz>
2025-03-12 23:56:49 +0100

    implement checkout functionality with user authentication, cart processing, and order summary display

--- App/Controllers/CheckoutController.php
+++ App/Controllers/CheckoutController.php
@@ -2,11 +2,42 @@
 
 namespace App\Controllers;
 
+use App\Models\Cache;
+
 class CheckoutController implements BaseController
 {
     public function handle()
     {
-        echo "";
+        $user = Cache::get('user');
+
+        if (!$user) {
+            header('Location: /product-catalog/login');
+            exit();
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $user->cart = [];
+            Cache::set('user', $user);
+            die('Thank you for your purchase!');
+        }
+        $products = Cache::get('products');
+        $cartProductsIds = $user->cart;
+        $groupedProducts = [];
+        foreach ($cartProductsIds as $id) {
+            if (!isset($groupedProducts[$id])) {
+                $groupedProducts[$id] = [
+                    'product' => $products[$id],
+                    'quantity' => 1
+                ];
+            } else {
+                $groupedProducts[$id]['quantity']++;
+            }
+        }
+
+        $total = 0;
+        foreach ($groupedProducts as $groupedProduct) {
+            $total += $groupedProduct['product']->price * $groupedProduct['quantity'];
+        }
+        return require_once './App/Views/CheckoutView.php';
     }    
 
 }
